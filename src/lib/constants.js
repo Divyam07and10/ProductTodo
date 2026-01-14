c44b56527b8ead465739c82147322ac429d8542c
Divyam07and10 <parhwal.divyam@thinksys.com>
2026-01-14 18:22:24 +0530

    Code Refactored

---
+++ src/lib/constants.js
@@ -0,0 +1,18 @@
+
+import BrushIcon from '@mui/icons-material/Brush';
+import DevicesIcon from '@mui/icons-material/Devices';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import HomeIcon from '@mui/icons-material/Home';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+
+export const CATEGORIES = [
+    { value: 'beauty', label: 'Beauty', icon: <BrushIcon fontSize="small" /> },
+    { value: 'electronics', label: 'Electronics', icon: <DevicesIcon fontSize="small" /> },
+    { value: 'fashion', label: 'Fashion', icon: <CheckroomIcon fontSize="small" /> },
+    { value: 'home', label: 'Home', icon: <HomeIcon fontSize="small" /> },
+    { value: 'sports', label: 'Sports', icon: <SportsSoccerIcon fontSize="small" /> },
+    { value: 'kids', label: 'Kids', icon: <ChildCareIcon fontSize="small" /> },
+    { value: 'food', label: 'Food', icon: <RestaurantIcon fontSize="small" /> },
+];
